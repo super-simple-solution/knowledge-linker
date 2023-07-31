f13a4eafd625a03a39d6184c34be7e6334388f52
wudaxue <1203588983@qq.com>
2023-08-01 00:15:27 +0800

    init

---
+++ src/service_worker.js
@@ -0,0 +1,15 @@
+// import { initEventHandler } from '@/utils/extension-action'
+
+// const contentReq = {
+//   'example': exampleFunc,
+// }
+
+// function exampleFunc(_, __, tabId) {
+// }
+
+// initEventHandler(contentReq)
+// chrome.action.onClick = function () {
+//   chrome.tabs.query({})
+// }
+
+console.log(1231)
