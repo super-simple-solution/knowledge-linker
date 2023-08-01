d696ea5131e703222d3d9c4e2ff6676f88af1a2e
wudaxue <1203588983@qq.com>
2023-08-02 02:21:01 +0800

    popup.js

--- src/content/index.js
+++ src/content/index.js
@@ -1,2 +1,7 @@
-function init() {}
+import { injectScriptByUrl } from '@/utils/extension-action.js'
+
 init()
+
+function init() {
+  injectScriptByUrl('https://unpkg.com/@popperjs/core@2/dist/umd/popper.js')
+}
