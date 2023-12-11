5101d142157048b21b8825630f3b75ed4b5d5ec7
zhangpan <zhangpanrobot@gmail.com>
2023-12-12 01:18:36 +0800

    mvp code

--- src/content/index.js
+++ src/content/index.js
@@ -5,3 +5,75 @@ init()
 function init() {
   injectScriptByUrl('https://unpkg.com/@popperjs/core@2/dist/umd/popper.js')
 }
+
+let wordId = 0
+// 停用词hash
+const stopWordsHashMap = {}
+// 当前文档不满足匹配词的hash
+const misMatchedHashMap = {}
+const matchedHashMap = {}
+const keyWordList = ['encode', 'stemmer']
+
+wrapTextNodes()
+
+function wrapTextNodes() {
+  let result = {
+    contentList: [],
+    singleWordCount: 0,
+  }
+
+  var treeWalker = document.createTreeWalker(
+    document.body, // 从body元素开始
+    NodeFilter.SHOW_TEXT, // 只遍历文本节点
+    (node) => (node.nodeValue.trim().length < 2 ? NodeFilter.FILTER_SKIP : NodeFilter.FILTER_ACCEPT),
+  )
+
+  while (treeWalker.nextNode()) {
+    // 遍历所有文本节点
+    var textNode = treeWalker.currentNode
+    const content = textNode.nodeValue.trim()
+    result.contentList.push(content)
+    // TODO: 避免encoder 匹配上 encode 关键词
+    if (!/\s/.test(content)) {
+      // 单词
+      result.singleWordCount++
+      let wrapper = matchKeyWord(content, textNode)
+      if (wrapper) {
+        textNode.parentNode.insertBefore(wrapper, textNode)
+        textNode.parentNode.removeChild(textNode)
+      }
+    } else {
+      // TODO
+      // 句子
+    }
+  }
+  return result
+}
+
+function matchKeyWord(word, textNode) {
+  if (misMatchedHashMap[word]) return
+  if (stopWordsHashMap[word]) {
+    misMatchedHashMap[word] = 0
+    return
+  }
+  if (matchedHashMap[word]) {
+    return markWord(word, textNode)
+  }
+  if (keyWordList.indexOf(word) !== -1) {
+    // id 递增
+    matchedHashMap[word] = Object.keys(matchedHashMap).length
+    return markWord(word, textNode)
+  } else {
+    misMatchedHashMap[word] = 0
+  }
+}
+
+function markWord(word, textNode) {
+  var wrapper = document.createElement('sss-hc') // 创建新的元素
+  var highlightedText = textNode.nodeValue.replace(
+    new RegExp(word, 'g'),
+    `<sss-highlight id="relin-${++wordId}" class="relingo-underline relinmark" data-word="${word}"><relin-origin>${word}</relin-origin></sss-highlight>`,
+  )
+  wrapper.innerHTML = highlightedText // 设置新元素的内容
+  return wrapper
+}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
+import zipPack from 'vite-plugin-zip-pack'
 import eslintPlugin from 'vite-plugin-eslint'
-import vue from '@vitejs/plugin-vue'
 import tailwind from 'tailwindcss'
 import manifest from './manifest.json'
-import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
@@ -16,27 +21,7 @@ export default defineConfig({
   output: {
     sourcemap: 'inline',
   },
-  plugins: [
-    vue(),
-    crx({ manifest }),
-    eslintPlugin(),
-    AutoImport({
-      dts: true,
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/, // .md
-      ],
-      imports: ['vue'],
-      eslintrc: {
-        enabled: true, // Default `false`
-        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-      },
-      resolvers: [() => null],
-    }),
-  ],
+  plugins: [crx({ manifest }), eslintPlugin(), zipPack({ outDir: './' })],
   css: {
     // https://github.com/vitejs/vite/discussions/8216
     modules: {
